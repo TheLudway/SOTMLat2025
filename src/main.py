7b498c8e3fca00071f62d62f85ff8c7ee0a729a6
TheLudway <theludway@gmail.com>
2025-09-05 15:36:49 -0500

    Python file

--- src/main.py
+++ src/main.py
@@ -15,7 +15,7 @@ def read_secret(path):
 
 db_user = os.getenv("POSTGRES_USER", "admin")
 db_name = os.getenv("POSTGRES_DB", "osmdata")
-db_host = os.getenv("POSTGRES_HOST", "localhost")
+db_host = os.getenv("POSTGRES_HOST", "192.168.1.8")
 db_port = os.getenv("POSTGRES_PORT", "5432")
 db_password = read_secret(os.getenv("POSTGRES_PASSWORD_FILE", "/run/secrets/postgres_password"))
 
